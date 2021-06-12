f453ed3a5dc5221fedcc15a644fce070ba80a659
yakshitgupta310 <65021783+yakshitgupta310@users.noreply.github.com>
2021-06-12 12:35:09 +0530

    Add files via upload

--- pattern-8.py
+++
@@ -1,8 +0,0 @@
-for i in range(9):
-    if i < 5:
-        for j in range(i + 1):
-            print("*", end=" ")
-    else:
-        for j in range(9 - i):
-            print("*", end=" ")
-    print()
